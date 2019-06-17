f6bdc3a7ef6d027106c26e9e94978e9c4c1e45fd
Darren Liu <butterfly@msrlab.org>
2019-06-17 16:49:36 +0800

    Added profiles for 江苏肿瘤医院.

--- src/main/java/com/gennlife/fs/configurations/HospitalNames.java
+++ src/main/java/com/gennlife/fs/configurations/HospitalNames.java
@@ -4,6 +4,7 @@ package com.gennlife.fs.configurations;
 public class HospitalNames {
 
     public static final String HOSPITAL_DEV = "巴甫洛夫·薛定谔宠物医院";
+    public static final String HOSPITAL_JSZLYY = "江苏肿瘤医院";
     public static final String HOSPITAL_JZYKDXDIFSYY = "锦州医科大学第一附属医院";
 
 }
--- src/main/java/com/gennlife/fs/service/UserService.java
+++ src/main/java/com/gennlife/fs/service/UserService.java
@@ -4,8 +4,7 @@ import com.gennlife.fs.configurations.GeneralConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import static com.gennlife.fs.configurations.HospitalNames.HOSPITAL_DEV;
-import static com.gennlife.fs.configurations.HospitalNames.HOSPITAL_JZYKDXDIFSYY;
+import static com.gennlife.fs.configurations.HospitalNames.*;
 
 @Service
 public class UserService {
@@ -13,6 +12,8 @@ public class UserService {
     public boolean isAllowedToAccessPatientPrivacyData(String userId) {
         switch (cfg.hospitalName) {
             case HOSPITAL_DEV:
+                return true;
+            case HOSPITAL_JSZLYY:
                 return true;
             case HOSPITAL_JZYKDXDIFSYY:
                 return "8ec80ec4-34f8-4acb-999a-4fc508406bde".equals(userId);
