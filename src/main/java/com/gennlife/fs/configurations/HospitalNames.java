85d1d73aa5677cb43fffac89d701c3e9fa765abc
Darren Liu <butterfly@msrlab.org>
2019-06-19 14:04:14 +0800

    Added 2 profiles.

--- src/main/java/com/gennlife/fs/configurations/HospitalNames.java
+++ src/main/java/com/gennlife/fs/configurations/HospitalNames.java
@@ -4,7 +4,11 @@ package com.gennlife.fs.configurations;
 public class HospitalNames {
 
     public static final String HOSPITAL_DEV = "巴甫洛夫·薛定谔宠物医院";
-    public static final String HOSPITAL_JSZLYY = "江苏肿瘤医院";
+
     public static final String HOSPITAL_JZYKDXDIFSYY = "锦州医科大学第一附属医院";
+    public static final String HOSPITAL_JSZLYY = "江苏肿瘤医院";
+    public static final String HOSPITAL_TJZLYY = "天津肿瘤医院";
+    public static final String HOSPITAL_XAJTDXDYFSYY = "西安交通大学第一附属医院";
+    public static final String HOSPITAL_ZZZXYY = "郑州中心医院";
 
 }
